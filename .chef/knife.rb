97c57bdb08cf9244c8b7510c4c244758c68371f8
Cloud Genius <user@cloudgeni.us>
2016-11-18 15:53:47 +0000

    initial commit

---
+++ .chef/knife.rb
@@ -0,0 +1,35 @@
+# See http://docs.chef.io/config_rb_knife.html for more information on knife configuration options
+
+current_dir = File.dirname(__FILE__)
+log_level                :info
+log_location             STDOUT
+node_name                "salam99"
+client_key               "#{current_dir}/salam99.pem"
+chef_server_url          "https://api.chef.io/organizations/aqib"
+cookbook_path ["#{current_dir}/../site-cookbooks","#{current_dir}/../cookbooks"]
+cache_type                "BasicFile"
+cache_options( :path => "#{ENV['HOME']}/.chef/checksums" )
+verify_api_cert            true
+
+cookbook_copyright        "Mohammed Salam"
+cookbook_email            "salam9920@gmail.com"
+cookbook_license          "apachev2"
+
+# https://console.aws.amazon.com/iam/home?region=us-east-1#security_credential
+
+knife[:aws_access_key_id]     = "AKIAJXGALLGNCXJJI33A"
+knife[:aws_secret_access_key] = "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"
+knife[:region]                = "us-west-2"
+
+# https://cloud.digitalocean.com/settings/applications
+
+knife[:digital_ocean_access_token] = "CHANGETHIS"
+
+# https://manage.windowsazure.com/publishsettings/index?client=xplat
+# make sure to rename the downloaded publishsettings file as myazure.publishsettings and place in .chef folder
+
+knife[:azure_publish_settings_file] = "#{current_dir}/myazure.publishsettings"
+knife[:azure_source_image] = "b39f27a8b8c64d52b05eac6a62ebad85__Ubuntu-14_04_4-LTS-amd64-server-20160627-en-us-30GB"
+
+#Uncomment this line after you have created encryption secret
+#knife[:secret_file] = "#{current_dir}/encrypted_data_bag_secret"
