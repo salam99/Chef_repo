b162344b7abb05924b34907cae9dc1fb54e86e30
Cloud Genius <user@cloudgeni.us>
2016-11-20 06:45:07 +0000

    Read MySQL passwords from encrypted data bag.

---
+++ site-cookbooks/yoursql/metadata.rb
@@ -0,0 +1,7 @@
+name             'yoursql'
+maintainer       'Mohammed Salam'
+maintainer_email 'salam9920@gmail.com'
+license          'Apache 2.0'
+description      'Installs/Configures yoursql'
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+version          '0.1.0'
